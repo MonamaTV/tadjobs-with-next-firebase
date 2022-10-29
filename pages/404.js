3ba20a2228e30a461acbed782580f83b6032271e
MonamaTV <keletsovincent92@gmail.com>
2022-10-29 12:05:48 +0200

    Errors

--- pages/404.js
+++ pages/404.js
@@ -8,7 +8,7 @@ const NotFound = () => {
       <img src="/assets/notfound.svg" />
       <div className="not_found_message">
         <h1>OPPS!</h1>
-        <h3>We couldn't find what you are looking for...</h3>
+        <h3>We couldn&apos;t find what you are looking for...</h3>
         <Link href="/">
           <a>Go back home</a>
         </Link>
